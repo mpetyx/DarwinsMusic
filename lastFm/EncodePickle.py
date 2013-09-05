bfe55daf75efc86f9a7c9fe5adccd08891477cfa
Michael Petychakis <mpetyx@gmail.com>
2013-09-05 16:37:17 +0300

    handling storing of triples

--- lastFm/EncodePickle.py
+++ lastFm/EncodePickle.py
@@ -1,6 +1,7 @@
 __author__ = 'mpetyx'
 
 import pickle
+import datetime, os, sys, re, time
 from rdflib import Graph, Literal, Namespace, URIRef, ConjunctiveGraph
 
 mbid = "6cc0ab41-adb5-457f-b90f-3e0992f7b5ff"
@@ -11,19 +12,34 @@ track = pickle.load(file("../data/"+mbid+".pickle", "r"))
 # print track.get_listener_count()
 # print track.get_top_tags()
 
-g = ConjunctiveGraph()
 
 MUSIC = Namespace('http://eswc.com#')
 
-trackuri = URIRef('http://musicbrainz.org/recording/%s' % mbid)
+storefn = os.path.expanduser('/Users/mpetyx/Desktop/projects/repos/TooLate/lastFm/tracks.n3')
+#storefn = '/home/simon/codes/film.dev/movies.n3'
+storeuri = 'file://'+storefn
 
-g.add((trackuri, MUSIC.has_playcount, Literal(track.get_playcount())))
+class Store:
+    def __init__(self):
+        self.graph = ConjunctiveGraph()
+        if os.path.exists(storefn):
+            self.graph.load(storeuri, format='n3')
+        self.graph.bind('MUSIC', MUSIC)
+        self.graph.bind('rev', 'http://purl.org/stuff/rev#')
 
-g.add((trackuri, MUSIC.has_listener_count, Literal(track.get_listener_count())))
+    def save(self):
+        self.graph.serialize(storeuri, format='n3')
 
-for tag in track.get_tags():
+    def track(self,mbid,track):
 
-    g.add((trackuri, MUSIC.has_tag, Literal(tag)))
+        trackuri = URIRef('http://musicbrainz.org/recording/%s' % mbid)
+        self.graph.add((trackuri, MUSIC.has_playcount, Literal(track.get_playcount())))
+        self.graph.add((trackuri, MUSIC.has_listener_count, Literal(track.get_listener_count())))
+        for tag in track.get_tags():
 
+            self.graph.add((trackuri, MUSIC.has_tag, Literal(tag)))
 
-print g.serialize(format='n3')
+        self.save()
+
+s = Store()
+s.track(mbid=mbid,track=track)
