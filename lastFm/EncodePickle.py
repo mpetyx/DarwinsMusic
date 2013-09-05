346ded937b42e0197d06a9ba95a0c81135c2de3e
Michael Petychakis <mpetyx@gmail.com>
2013-09-05 17:05:41 +0300

    generalizing the n3 mechanism

--- lastFm/EncodePickle.py
+++ lastFm/EncodePickle.py
@@ -1,23 +1,16 @@
 __author__ = 'mpetyx'
 
 import pickle
-import datetime, os, sys, re, time
-from rdflib import Graph, Literal, Namespace, URIRef, ConjunctiveGraph
+import os
+from rdflib import Literal, Namespace, URIRef, ConjunctiveGraph, RDF
 
 mbid = "6cc0ab41-adb5-457f-b90f-3e0992f7b5ff"
-
-track = pickle.load(file("../data/"+mbid+".pickle", "r"))
-
-# print track.get_playcount()
-# print track.get_listener_count()
-# print track.get_top_tags()
-
 
 MUSIC = Namespace('http://eswc.com#')
 
 storefn = os.path.expanduser('/Users/mpetyx/Desktop/projects/repos/TooLate/lastFm/tracks.n3')
-#storefn = '/home/simon/codes/film.dev/movies.n3'
-storeuri = 'file://'+storefn
+storeuri = 'file://' + storefn
+
 
 class Store:
     def __init__(self):
@@ -30,16 +23,33 @@ class Store:
     def save(self):
         self.graph.serialize(storeuri, format='n3')
 
-    def track(self,mbid,track):
+    def track(self, mbid, track):
 
         trackuri = URIRef('http://musicbrainz.org/recording/%s' % mbid)
+        self.graph.add((trackuri, RDF.type, MUSIC['Track']))
         self.graph.add((trackuri, MUSIC.has_playcount, Literal(track.get_playcount())))
         self.graph.add((trackuri, MUSIC.has_listener_count, Literal(track.get_listener_count())))
         for tag in track.get_tags():
-
             self.graph.add((trackuri, MUSIC.has_tag, Literal(tag)))
 
         self.save()
 
-s = Store()
-s.track(mbid=mbid,track=track)
+    def track_is_in(self, uri):
+        return (URIRef(uri), RDF.type, MUSIC['Track']) in self.graph
+
+
+if __name__ == "__main__":
+
+    s = Store()
+    file_with_mbids = open("list_of_mbid.txt","r")
+    mbids = file_with_mbids.readlines()
+
+    for mbid in mbids:
+        mbid = mbid.replace("\n","")
+        track = pickle.load(file("../data/" + mbid + ".pickle", "r"))
+        trackuri = URIRef('http://musicbrainz.org/recording/%s' % mbid)
+        if s.track_is_in(trackuri):
+            print "it already exist!"
+        else:
+            s.track(mbid=mbid, track=track)
+            print "i saved a new one!"
