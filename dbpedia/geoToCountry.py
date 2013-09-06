b4439ef76148f73581e6df0bf593504ae796578a
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2013-09-06 19:23:36 +0300

    correct a bug in geo to country code.

--- dbpedia/geoToCountry.py
+++ dbpedia/geoToCountry.py
@@ -4,5 +4,7 @@ from urllib2 import urlopen
 def getCountry(lat, lng):
     url = "http://ws.geonames.org/countryCode?lng=" + str(lng) + "&lat=" + str(lat)
     country = urlopen(url).read().strip()
+	if len(country) != 2:
+		return "Unknown"
     return country
 
