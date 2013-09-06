9f3e67cf5d7008f23a28d995e4977832ed23a1d3
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2013-09-06 16:44:52 +0300

    added geo code to country script

--- lastFm/getRecordingInfo.py
+++ lastFm/getRecordingInfo.py
@@ -6,26 +6,22 @@ from glob import glob
 from os.path import basename, exists
 from time import sleep
 
-import lxml
-
-
-files = glob("../data/jazz/*.json")
+tag = 'rock'
+files = glob("../data/rock/toptracks/*.json")
 
 for f in files:
     track = json.load(file(f))
     if track['mbid'] == '':
         mbid = basename(f)[:-5]
         print mbid, track['artist']['name'], track['name']
-        url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=9d8eec456625c74e02a7e23bd1d7c68a&artist=" + quote(
-            track['artist']['name'].encode('utf-8')) + "&track=" + quote(track["name"].encode('utf-8')) + "&format=json"
+        url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=9d8eec456625c74e02a7e23bd1d7c68a&artist=" + quote(track['artist']['name'].encode('utf-8')) + "&track=" + quote(track["name"].encode('utf-8')) + "&format=json"
     else:
         mbid = track['mbid']
-        url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=9d8eec456625c74e02a7e23bd1d7c68a&mbid=" +
-              track['mbid'] + "&format=json"
+        url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=9d8eec456625c74e02a7e23bd1d7c68a&mbid=" + track['mbid'] + "&format=json"
     if exists("../data/" + mbid + ".json"): continue
 
     print mbid
     trackInfo = urlopen(url).read()
     trackInfo = json.loads(trackInfo)
-    json.dump(trackInfo, file("../data/" + mbid + ".json", 'w'))
-    sleep(0.5)
+    json.dump(trackInfo, file("../data/" + tag + "/trackInfo/" + mbid + ".json", 'w'))
+    sleep(0.25)
--- lastFm/getRecordings.py
+++ lastFm/getRecordings.py
@@ -7,14 +7,13 @@ from urllib2 import urlopen
 import lxml
 
 
-tag = "jazz"
+tag = "rock"
 pageLimit = 50
 pages = 100
 
+
 for page in xrange(pages):
-    data = urlopen(
-        "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=" + tag + "&page=" + str(page) + "&limit=" + str(
-            pageLimit) + "&api_key=9d8eec456625c74e02a7e23bd1d7c68a&format=json").read()
+    data = urlopen("http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=" + tag + "&page=" + str(page) + "&limit=" + str(pageLimit) + "&api_key=9d8eec456625c74e02a7e23bd1d7c68a&format=json").read()
     data = json.loads(data)
     for track in data['toptracks']['track']:
         if track['mbid'] == '':
@@ -22,4 +21,4 @@ for page in xrange(pages):
         else:
             mbid = track['mbid']
         print mbid
-        json.dump(track, file("../data/" + tag + "/" + mbid + ".json", 'w'))
+        json.dump(track, file("../data/" + tag + "/toptracks/" + mbid + ".json", 'w'))
