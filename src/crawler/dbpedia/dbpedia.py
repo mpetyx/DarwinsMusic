790ec2a1e688576336a08a2c6997a969db8419d5
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2013-09-19 23:37:47 +0200

    Added functions to get influences of artists from DBPedia

--- src/crawler/dbpedia/dbpedia.py
+++ src/crawler/dbpedia/dbpedia.py
@@ -6,12 +6,9 @@ from time import time
 import pickle
 import json
 
-#TODO - 1
+#TODO
 #In these functions, we need to work out the cases which have more than
 #one result. For now, I consider the first hit!
-
-#TODO - 2
-#Get influence relations
 
 class dbpedia:
     def __init__(self, delay = 0.5):
@@ -27,6 +24,73 @@ class dbpedia:
         if actualDelay < self.delay: sleep(actualDelay)
         self.lastRequest = time()
     
+    def getInfluences(self, artisturi):
+        """
+        Gets the list of artists who influenced the given artist
+        """
+        self.sparql.setQuery("""
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        PREFIX : <http://dbpedia.org/resource/>
+        PREFIX dbpedia2: <http://dbpedia.org/property/>
+        PREFIX dbpedia: <http://dbpedia.org/>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+        PREFIX dbpprop: <http://dbpedia.org/property/>
+
+        SELECT ?influences WHERE {
+         { %s dbpedia-owl:influencedBy ?influencedBy } UNION { ?influencedBy dbpprop:influences %s }
+         }
+        """%artisturi)
+
+        self.sparql.setReturnFormat(JSON)
+        self.wait()
+        results = self.sparql.query().convert()
+
+        if results['results']['bindings'] == []:
+            return None
+        else:
+            results = results['results']['bindings']
+        return results
+
+
+    def getInfluenced(self, artisturi):
+        """
+        Gets the list of artists who are influenced by the given artist
+        """
+        self.sparql.setQuery("""
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        PREFIX : <http://dbpedia.org/resource/>
+        PREFIX dbpedia2: <http://dbpedia.org/property/>
+        PREFIX dbpedia: <http://dbpedia.org/>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+        PREFIX dbpprop: <http://dbpedia.org/property/>
+
+        SELECT ?influences WHERE {
+         { ?influenced dbpedia-owl:influencedBy %s } UNION { %s dbpprop:influences ?influenced }
+         }
+        """%artisturi)
+
+        self.sparql.setReturnFormat(JSON)
+        self.wait()
+        results = self.sparql.query().convert()
+
+        if results['results']['bindings'] == []:
+            return None
+        else:
+            results = results['results']['bindings']
+        return results
+
     def getBand(self, name):
         """
         Gets band information from DBPedia, given the name of a band.
@@ -50,7 +114,8 @@ class dbpedia:
 
         SELECT ?artist, ?careerStartYear, ?hometown, ?hometownName, ?coordinates WHERE {
          ?artist rdf:type <http://schema.org/MusicGroup> .
-         ?artist foaf:name "%s"@en .
+         ?artist foaf:name ?name .
+         FILTER (STR(?name)="%s") .
          OPTIONAL { ?artist dbpedia-owl:activeYearsStartYear ?startYear }
          OPTIONAL { ?artist dbpedia-owl:hometown ?hometown }
          OPTIONAL { ?hometown foaf:name ?hometownName }
@@ -60,9 +125,7 @@ class dbpedia:
         """%name)
 
         self.sparql.setReturnFormat(JSON)
-
         self.wait()
-
         results = self.sparql.query().convert()
 
         if results['results']['bindings'] == []:
@@ -96,7 +159,8 @@ class dbpedia:
 
         SELECT ?artist, ?careerStartYear, ?hometown, ?hometownName, ?coordinates, ?influenced, ?influencedBy WHERE {
          ?artist rdf:type dbpedia-owl:MusicalArtist .
-         ?artist foaf:name "%s"@en .
+         ?artist foaf:name ?name .
+         FILTER (STR(?name)="%s") .
          OPTIONAL { ?artist dbpedia-owl:activeYearsStartYear ?careerStartYear }
          OPTIONAL { ?artist dbpedia-owl:birthPlace ?hometown }
          OPTIONAL { ?hometown foaf:name ?hometownName }
@@ -106,14 +170,9 @@ class dbpedia:
         limit 2
         """%name)
 
-        #OPTIONAL { { ?artist dbpedia-owl:influencedBy ?influencedBy } UNION { ?influencedBy dbpprop:influences ?artist } }
-        #OPTIONAL { { ?influenced dbpedia-owl:influencedBy ?artist } UNION { ?artist dbpprop:influences ?influenced } }
         self.sparql.setReturnFormat(JSON)
-
         self.wait()
-        
         results = self.sparql.query().convert()
-        return results
 
         if results['results']['bindings'] == []:
             results = self.getBand(name)
